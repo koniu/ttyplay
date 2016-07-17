6f18c76baba2433521e754058c82b84ec81d7bd8
koniu <koniu@riseup.net>
2016-07-17 09:48:25 +0100

    whitespace cleanup

--- main.c
+++ main.c
@@ -35,11 +35,11 @@
 #define BAUDRATE 115200
 #define SERIAL_BAUDRATE B115200
 
-int main(int argc, char **argv) {  
-  
+int main(int argc, char **argv) {
+
   SNDFILE *soundfile = NULL;
   SF_INFO info;
-  
+
   SNDFILE *soundfile_out = NULL;
   SF_INFO info_out;
 
@@ -53,9 +53,9 @@ int main(int argc, char **argv) {
 
   int ttyfd=-1;
   struct termios oldtio,newtio;
-  
+
   unsigned char outbyte;
-  
+
   SRC_STATE *samplerate;
   SRC_DATA resample_data;
   SRC_STATE *samplerate_out;
@@ -115,28 +115,27 @@ int main(int argc, char **argv) {
       return 1;
     }
   } else { // we're not writing an output file, try serial I/O
-
     // blocking I/O blocks forever on open(), dunno why.
     ttyfd = open(device, O_RDWR|O_NOCTTY|O_NONBLOCK);
     if (ttyfd <0) {perror(device); exit(-1); }
     tcgetattr(ttyfd,&oldtio); /* save current serial port settings */
     bzero(&newtio, sizeof(newtio)); /* clear struct for new port settings */
-    
+
     cfmakeraw(&newtio);
     if (0!=cfsetospeed(&newtio,SERIAL_BAUDRATE)) { perror(device); exit(-1); }
     if (0!=tcflush(ttyfd, TCIFLUSH)) { perror(device); exit(-1); }
     if (0!=tcsetattr(ttyfd,TCSANOW,&newtio)) { perror(device); exit(-1); }
   }
-  
+
   samplerate=src_new(SRC_LINEAR,1,&error);
 
   if (error) {
     fprintf(stderr,"Samplerate error. Sorry.\n");
     return 1;
   }
-  
+
   samplerate_out=src_new(SRC_SINC_FASTEST,1,&error);
-  
+
   if (error) {
     fprintf(stderr,"Samplerate error. Sorry.\n");
     return 1;
@@ -163,49 +162,49 @@ int main(int argc, char **argv) {
     for (j=0; j<count; j++) {
       resample_data.data_in[j]=resample_data.data_in[j*info.channels];
       for (i=1; i<info.channels; i++) {
-	resample_data.data_in[j]+=resample_data.data_in[j*info.channels+i];
-      }      
+        resample_data.data_in[j]+=resample_data.data_in[j*info.channels+i];
+      }
       resample_data.data_in[j]/=(float)info.channels;
     }
 
     resample_data.input_frames=count;
-    
+
     // following has a bug...
     if (count<(int)(SOUND_BUFFER_LEN*info.samplerate)) {
       resample_data.end_of_input=1;
       resample_data_out.end_of_input=1;
     }
-    
+
     src_process(samplerate,&resample_data);
-    
+
     // Now we have our input signal converted to the serial baudrate.  All that's left is sigma-delta modulate
     // and write to the serial port
 
     outbyte=0;
     for (j=0; j<resample_data.output_frames_gen; j++) {
- 
+
       error_signal=resample_data.data_out[j]-out;
-      
+
       integrator+=error_signal;
 
       switch (k%10) {
       case 0:
-      	outbyte=0;
-	out=-1.0; //start bit
-	break;
+        outbyte=0;
+        out=-1.0; //start bit
+        break;
       case 9:
-	out=1.0; //stop bit
-	if (ttyfd>=0) {
-	  while ( 1!=write(ttyfd,&outbyte,1)) { /* What's a spinloop among friends? */ ; }
-	}
-	break;
+        out=1.0; //stop bit
+        if (ttyfd>=0) {
+          while ( 1!=write(ttyfd,&outbyte,1)) { /* What's a spinloop among friends? */ ; }
+        }
+        break;
       default:
-	out=(integrator>0) ? 1.0 : -1.0;
-	if (out>0.0) outbyte |= 1<<((k%10)-1);	
+        out=(integrator>0) ? 1.0 : -1.0;
+        if (out>0.0) outbyte |= 1<<((k%10)-1);
       }
 
       resample_data_out.data_in[j]=out;
-      
+
       k++;
     }
 
@@ -214,8 +213,8 @@ int main(int argc, char **argv) {
       src_process(samplerate_out,&resample_data_out);
 
       i=sf_write_float(soundfile_out,
-		       resample_data_out.data_out,
-		       resample_data_out.output_frames_gen);
+        resample_data_out.data_out,
+        resample_data_out.output_frames_gen);
 
     }
 
